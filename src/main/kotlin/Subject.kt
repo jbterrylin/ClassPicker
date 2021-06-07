200a7a76d25e382164bdfb6e40cb62232bd47f0d
jb terry lin <47049114+jbterrylin@users.noreply.github.com>
2021-06-08 02:25:43 +0800

    done subject combination algorithm

--- src/main/kotlin/Subject.kt
+++ src/main/kotlin/Subject.kt
@@ -6,53 +6,91 @@ class Subject {
     var chained = mutableListOf<MutableList<String>>()
     var preferred = mutableListOf<String>()
 
-    // Extensions
-    fun <T, S> Collection<T>.cartesianProduct(other: Iterable<S>): List<Pair<T, S>> {
-        return cartesianProduct(other) { first, second -> first to second }
-    }
-
-    private fun <T, S, V> Collection<T>.cartesianProduct(other: Iterable<S>, transformer: (first: T, second: S) -> V): List<V> {
-        return this.flatMap { first -> other.map { second -> transformer.invoke(first, second) } }
-    }
-
-    fun combineTwoList(list1: MutableList<SubjectTime>,list2: MutableList<SubjectTime>): MutableList<MutableList<SubjectTime>> {
-//        val list1 = subjectTimes.filter { it.group == mutableEntry.key }.toMutableList()
-//        val list2 = subjectTimes.filter { it.group == mutableEntry.key }.toMutableList()
-        val tempTimes = mutableListOf<MutableList<SubjectTime>>()
-        list1.cartesianProduct(list2) { a, b ->
-            if(!(tempTimes.any { it == mutableListOf<SubjectTime>(a, b) } ||
-                        tempTimes.any { it == mutableListOf<SubjectTime>(b, a) }))
-                tempTimes.add(mutableListOf<SubjectTime>(a, b))
+    /* arr[]  ---> Input Array
+    data[] ---> Temporary array to store current combination
+    start & end ---> Staring and Ending indexes in arr[]
+    index  ---> Current index in data[]
+    r ---> Size of a combination to be printed */
+    private fun combinationUtil (
+        arr: Array<SubjectTime>, data: Array<SubjectTime>, start: Int,
+        end: Int, index: Int, r: Int, result: MutableList<MutableList<SubjectTime>>
+    ):MutableList<MutableList<SubjectTime>> {
+        // Current combination is ready to be printed, print it
+        if (index == r) {
+            result.add(data.toMutableList())
+            return result
         }
-        tempTimes.removeAll { it.distinct().size != it.size }
-        for(tempTime in tempTimes)
-            println(tempTime[0].location + " " +tempTime[1].location)
-        return tempTimes
+
+        // replace index with all possible elements. The condition
+        // "end-i+1 >= r-index" makes sure that including one element
+        // at index will make a combination with remaining elements
+        // at remaining positions
+        var i = start
+        while (i <= end && end - i + 1 >= r - index) {
+            data[index] = arr[i]
+            combinationUtil(arr, data, i + 1, end, index + 1, r, result)
+            i++
+        }
+        return result
+    }
+
+    private fun combination(arr: Array<SubjectTime>, n: Int, r: Int): MutableList<MutableList<SubjectTime>> {
+        return combinationUtil(arr, Array(r, init= { SubjectTime() }), 0, n - 1, 0, r, mutableListOf())
+    }
+    private fun combinationUtil2dList (
+        arr: Array<MutableList<SubjectTime>>, data: Array<MutableList<SubjectTime>>, start: Int,
+        end: Int, index: Int, r: Int, result: MutableList<MutableList<MutableList<SubjectTime>>>
+    ):MutableList<MutableList<MutableList<SubjectTime>>> {
+        // Current combination is ready to be printed, print it
+        if (index == r) {
+            result.add(data.toMutableList())
+            return result
+        }
+
+        // replace index with all possible elements. The condition
+        // "end-i+1 >= r-index" makes sure that including one element
+        // at index will make a combination with remaining elements
+        // at remaining positions
+        var i = start
+        while (i <= end && end - i + 1 >= r - index) {
+            data[index]=(arr[i])
+            combinationUtil2dList(arr, data, i + 1, end, index + 1, r, result)
+            i++
+        }
+        return result
+    }
+
+    private fun combination2dList(arr: Array<MutableList<SubjectTime>>, n: Int, r: Int): MutableList<MutableList<MutableList<SubjectTime>>> {
+        return combinationUtil2dList(arr, Array(r, init= { mutableListOf() }), 0, n - 1, 0, r, mutableListOf())
     }
 
     fun combineTime() {
-        var timeList = mutableListOf<MutableList<MutableList<SubjectTime>>>()
+        val subjectTimeList = mutableListOf<MutableList<SubjectTime>>()
+        val timeList = mutableListOf<MutableList<MutableList<SubjectTime>>>()
         for (mutableEntry in required) {
-            if(mutableEntry.value == 1) {
-                timeList.add(subjectTimes.filter { it.group == mutableEntry.key }.map { mutableListOf<SubjectTime>(it) }.toMutableList())
-            } else {
-                var tempTimeList = mutableListOf<MutableList<MutableList<SubjectTime>>>()
-                combineTwoList(
-                    subjectTimes.filter { it.group == mutableEntry.key }.toMutableList(),
-                    subjectTimes.filter { it.group == mutableEntry.key }.toMutableList())
-            }
+            val arr = subjectTimes.filter { it.group == mutableEntry.key }.toTypedArray()
+            val r = mutableEntry.value
+            val n = arr.size
+            val a = combination(arr, n, r)
+            timeList.add(a)
         }
-//        val ints = listOf(0, 1, 2)
-//        val strings = listOf("a", "b", "c")
-//        strings.cartesianProduct(ints) { string, int ->
-//            "$int $string"
-//        }
-//
-//        // Or use more generic one
-//        strings.cartesianProduct(ints)
-//            .map { (string, int) ->
-//                "$int $string"
-//            }
-//            .forEach(::println)
+
+        val arr = timeList.flatten().toTypedArray()
+        val r = required.size
+        val n = arr.size
+        val groupedTimeList = combination2dList(arr, n, r)
+
+        // remove clash time combination
+
+        for(groupedTimes in groupedTimeList) {
+            val groups = groupedTimes.map { it[0].group }.toMutableList()
+            if(groups.distinct().size == groups.size)
+                subjectTimeList.add(groupedTimes.flatten().toMutableList())
+        }
+        println(subjectTimeList.size)
+        for(a in subjectTimeList) {
+            println(a.map { it.location })
+        }
+        println("end")
     }
 }
